122e95ea106319436fb52a5ebb5c2ad9a55c81c2
Georgie Conway <dopestantelope@gmail.com>
2023-03-17 12:03:24 +0000

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return(
+    <nav className="flex justify-between m-10">
+      <h1 className="text-4xl">Art Studio</h1>
+      <button className="">Cart 0 Items</button>
+    </nav>
+  )
+}
+
+export default Navbar
